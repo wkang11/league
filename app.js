495a573280c83363ee4e1d8727857f2cb7601ba8
Weijie Kang <Weijie.Kang@hunterindustries.com>
2016-08-29 17:04:12 -0700

    add file into static

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 
 var server = http.createServer(app);
 
+
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
@@ -21,13 +22,14 @@ app.get('/home', function(req, res){
     res.sendFile(path.join(__dirname + '/pages/home.html'));
 });
 
+app.use(express.static(__dirname + '/static'));
 
 
-app.use(function(req, res, next){
-    var err = new Error('Batman said you are wrong');
-    err.status = 404;
-    next(err);
-});
+// app.use(function(req, res, next){
+//     var err = new Error('Batman said you are wrong');
+//     err.status = 404;
+//     next(err);
+// });
 
 server.listen(3000);
 server.on('error', onError);
