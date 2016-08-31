de66c3059bac42d4d62546b7146dc6363b9eb9fb
Weijie Kang <Weijie.Kang@hunterindustries.com>
2016-08-30 17:08:36 -0700

    edited home page

--- static/angular.js
+++ static/angular.js
@@ -11,19 +11,36 @@ app.config(function ($routeProvider) {
             controller: 'aboutController'
         })
         .when('/contact', {
-            templateUrl: 'pages/contact/html',
+            templateUrl: 'pages/contact.html',
             controller: 'contactController'
         });
 });
 
-app.controller('myController', function ($scope) {
+app.controller('myController', function ($scope, $http) {
+    $scope.doesSummonerExist = true;
     $scope.message = 'Let me start from beginning';
+    var pathArray = window.location.pathname.split('/');
+    console.log(pathArray);
+    var levelLocation = pathArray[2];
+    var url = "api/summoner/basic/" + levelLocation;
+    $http.get(url).then(function successCallback(data) {
+        $scope.doesSummonerExist = true;
+        for (var key in data.data) {
+            $scope.summoner = {
+                "id": data.data[key].id,
+                "name": data.data[key].name,
+                "iconId": data.data[key].profileIconId,
+                "revData": data.data[key].revisionDate,
+                "summonerLevel": data.data[key].summonerLevel
+            }
+        }
+    });
 });
 
-app.controller('aboutController', function($scope){
-    $scope.mesage = 'this is about page';
+app.controller('aboutController', function ($scope) {
+    $scope.message = 'this is about page';
 });
 
-app.controller('contactController', function($scope){
+app.controller('contactController', function ($scope) {
     $scope.message = 'You can contact Batman, and he will tell me your request.'
 });
